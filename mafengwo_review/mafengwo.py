22e292e877efdee5f83ab0818a4dbde4195d08e8
zheng_zeng <764452708@qq.com>
2017-10-11 22:32:09 +0800

    modified:   mafengwo_review/mafengwo.py

--- mafengwo_review/mafengwo.py
+++ mafengwo_review/mafengwo.py
@@ -11,29 +11,80 @@ header = {
 'upgrade-insecure-requests':'1',
 'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
-
 import requests
 import json
 from bs4 import BeautifulSoup
 s = requests.session()
 s.headers = header
 def getData(url = 'http://www.mafengwo.cn/i/7618004.html'):
+    data = {}
     r = s.get(url)
     r.encoding='utf-8'
     b = BeautifulSoup(r.text)
     userinfo = json.loads(b.find('script', type='text/javascript').text.strip().split('=',1)[1].strip().rstrip(';'))
     author_name =  userinfo.get('author_name','')
+    data['author_name'] = author_name
+
     avatar = userinfo.get('logo_120','')
-    day_time_people = dict([i.get('class')+[i.text.split('/',1)[1]] for i in b.find('div', class_='tarvel_dir_list clearfix').ul.children if not isinstance(i,str)])
+    data['avatar'] = avatar
+    try:
+        day_time_people = dict([i.get('class')+[i.text.split('/',1)[1]] for i in b.find('div', class_='tarvel_dir_list clearfix').ul.children if not isinstance(i,str)])
+        data.update(day_time_people)
+    except:
+        print('E!'+"day_time_people")
     article = [i for i in b.find('div', class_='va_con _j_master_content').contents if not isinstance(i,str)]
+    contents = []
     for i in article:
-        c = i.get('class')
-        if '_j_note_content' in c:
-            i.text
-            # pass
-        elif 'add_pic' in c:
-            i.img.get('data-rt-src')
-            # pass
-        elif 'article_title' in c:
-            i.h2.text
-            # pass
+        try:
+            c = i.get('class')
+            if '_j_note_content' in c:
+                contents.append(i.text.strip())
+                # pass
+            elif 'add_pic' in c:
+                contents.append('<img src="'+ i.img.get('data-rt-src').strip()+'"/>')
+                # pass
+            elif 'article_title' in c:
+                contents.append("<h2>"+ i.h2.text.strip() +"</h2>")
+                # pass
+        except:
+            print("airtile EEE!")
+    content  = '\n'.join(contents)
+    data['content'] = content
+
+    b2 =BeautifulSoup(json.loads(s.get('http://pagelet.mafengwo.cn/note/pagelet/headOperateApi?params={"iid":"%s"}'%userinfo['iid']).text)['data']['html'])
+    try:
+        time_view_like = [i.text for i in b2.find("div", class_='vc_time').contents if not isinstance(i, str)]
+        time = time_view_like[0]
+        data['time'] = time
+        view,like = time_view_like[1].split('/',1)
+        data['view'] = view
+        data['like'] = like
+    except:
+        print("EEE! time_view_like")
+    try:
+        destination = b.find('a', class_='_j_mdd_stas').text
+        data['destination'] = destination
+    except:
+        print("EEE! destination")
+    try:
+        b3 = BeautifulSoup(json.loads(s.get('http://pagelet.mafengwo.cn/note/pagelet/bottomReplyApi?params={"iid":"%s","page":"1"}'%userinfo['iid']).text)['data']['html'])
+        replys = b3.findAll('div', class_='mfw-cmt _j_reply_item')
+        reply_info =[]
+        for reply in replys:
+            reply_user_info = reply.find('div',class_="mcmt-info")
+            reply_user_name = reply_user_info.a.get("title")
+            reply_user_id = reply_user_info.a.get("href")
+            reply_user_avatar = reply_user_info.img.get("src")
+            reply_content = reply.find('p', class_='_j_reply_content').get("data-content")
+            reply_time = reply.find('div', class_='time').text
+            reply_info.append({'reply_user_name':reply_user_name,
+                               'reply_user_id':reply_user_id,
+                               'reply_user_avatar':reply_user_avatar,
+                               'reply_content':reply_content,
+                               'reply_time':reply_time})
+        data['reply_info'] = reply_info
+    except:
+        print("EEE! reply_info")
+    return  data
+# 'http://www.mafengwo.cn/note/detail.php?iId=7542569&iPage=2'
+# 'http://pagelet.mafengwo.cn/note/pagelet/recommendNoteApi?params={"type":0,"objid":0,"page":250,"ajax":1,"retina":0}'
